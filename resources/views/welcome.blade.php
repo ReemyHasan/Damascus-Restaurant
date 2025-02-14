cda57e35fd534066d247fd58982a8e756a2285b5
reem.hasan.831 <reema.samer.815@gmail.com>
2025-02-14 19:15:50 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
     <meta property="og:title" content="{{ $frontends['seo_data']->values['title'] }}">
 <meta property="og:description" content="{{ $frontends['seo_data']->values['description'] }}">
-<meta property="og:image" content="{{ $frontends['seo_data']->image->url ?? $frontends['hero_section']->image->url) }}">
+<meta property="og:image" content="{{ $frontends['seo_data']->image->url ?? $frontends['hero_section']->image->url }}">
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:type" content="website">
 <meta property="og:site_name" content="{{ $frontends['seo_data']->values['title'] }}">
